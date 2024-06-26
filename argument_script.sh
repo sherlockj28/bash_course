d7a1cee1f17035e00394ffd2b785741ba627aee6
John Sherlock <john.d.sherlock@gmail.com>
2024-06-26 10:47:44 -0500

    added additional comments to arguments text file and an if statement to the script.

--- argument_script.sh
+++ argument_script.sh
@@ -6,5 +6,12 @@
 
 lines=$(ls -ls -lh $5 | wc -l)
 
+if [ $# -ne 1 ]
+then
+    echo "The script requires exactly one directory path passed to it."
+    echo "Please try again."
+    exit 1
+fi
+
 echo "You have $(($lines-1)) objects in the $5 directory."
 
